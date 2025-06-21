3a52d21ac4aa097c0ed4e85697af766be1a78fc3
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-06-21 11:15:11 +0700

    Initial Commit

---
+++ AlarmKit-Example/AlarmKit_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmKit_ExampleApp.swift
+//  AlarmKit-Example
+//
+//  Created by Nonprawich I. on 21/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AlarmKit_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
